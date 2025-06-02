4f6e91fd19600b6dc43cef3192b2d7fa328b909f
faber.munoz <jhonfaber0803@gmail.com>
2025-06-01 19:20:36 -0500

    Barra de navegacion

--- futbol-club-fan/app/Providers/AppServiceProvider.php
+++ futbol-club-fan/app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         if (env('APP_ENV') == 'pdn') {
+         if (env('APP_ENV') == 'production') {
             $this->app['request']->server->set('HTTPS', true);
         }
     }
